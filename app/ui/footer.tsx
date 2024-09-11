f93a885f0ef207e4ed73d1da7176004df3ae2d47
Yander alves <yanderceara@gmail.com>
2024-09-11 17:37:46 -0300

    feat: Implementação da seção de parceria

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -5,44 +5,41 @@ import { legal, links, social } from '../lib/arrays'
 export default function Footer() {
   return (
     <footer className="w-full bg-primary">
-      <div className="flex flex-col justify-evenly w-4/5 mx-auto space-y-5">
-        <div className="space-y-4">
+      <div className="flex flex-col justify-evenly md:w-full space-y-5 p-5 md:flex-row md:justify-around">
+        <div className="space-y-4 flex justify-center items-center flex-col">
           <Image
             src="/logo.svg"
             alt="Logo do escritório de projetos"
             width={40}
             height={40}
           />
-          <p className="max-w-80">
+          <p className="max-w-80 text-center">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Reprehenderit, blanditiis iusto?
           </p>
-        </div>
-        <div>
-          <p className="text-gray-500 font-semibold text-sm">Home</p>
-          <ul>
-            {links.map((item) => (
-              <li className="font-bold" key={item.name}>
-                {item.name}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <p className="text-gray-500 font-semibold text-sm">Social</p>
-          <ul>
+          <ul className="flex space-x-7">
             {social.map((item) => (
-              <li className="font-bold" key={item.name}>
+              <li key={item.name}>
+                {<item.icon color="gray" width={30} height={30} />}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p className="text-gray-500 font-semibold text-lg">Home</p>
+          <ul className="flex flex-col space-y-3 mt-1">
+            {links.map((item) => (
+              <li className="font-bold text-xl" key={item.name}>
                 {item.name}
               </li>
             ))}
           </ul>
         </div>
         <div>
-          <p className="text-gray-500 font-semibold text-sm">Legal</p>
-          <ul>
+          <p className="text-gray-500 font-semibold text-lg">Legal</p>
+          <ul className="flex flex-col space-y-3 mt-1">
             {legal.map((item) => (
-              <li className="font-bold" key={item.name}>
+              <li className="font-bold text-xl" key={item.name}>
                 {item.name}
               </li>
             ))}
@@ -53,13 +50,6 @@ export default function Footer() {
         <p className="text-center">
           © 2024 Escritório de Projetos e Parcerias. All rights reserved.
         </p>
-        <ul className="flex space-x-7">
-          {social.map((item) => (
-            <li key={item.name}>
-              {<item.icon color="gray" width={30} height={30} />}
-            </li>
-          ))}
-        </ul>
       </div>
     </footer>
   )
